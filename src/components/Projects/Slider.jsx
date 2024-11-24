37d68f8407fad11c5da6d1f919b5cbea25fe048e
Clinton Mantchou Fezeu <clintonfezeu00@gmail.com>
2024-11-25 00:46:11 +0300

    debugging

---
+++ src/components/Projects/Slider.jsx
@@ -0,0 +1,123 @@
+import React, { useRef } from 'react';
+import Slider from 'react-slick';
+import Project from './Project';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+import styled from 'styled-components';
+
+let data = [
+  {
+    img: '/todoapp.PNG',
+    disc: 'The app to help organize and work on events',
+    url: 'https://clinton-fezeu.github.io/todoapp/',
+  },
+  {
+    img: '/royalwedding.PNG',
+    disc: 'Royal Wedding is famous for several dance sequences, including a classic number in which Astaire appears to dance on the ceiling and walls of his hotel room.',
+    url: 'https://clinton-fezeu.github.io/royalwedding/',
+  },
+  {
+    img: '/komatsufamily.PNG',
+    disc: 'Genta Komatsu: Engineer, Family Man, and Beloved Husband',
+    url: 'https://clinton-fezeu.github.io/komatsufamily/',
+  },
+  {
+    img: '/ferrisfamily.PNG',
+    disc: 'Kathleen Ferris: Trailblazer, Devoted Mother, and Lifelong Learner',
+    url: 'https://clinton-fezeu.github.io/ferrisfamily/',
+  },
+  {
+    img: '/curbsidethai.PNG',
+    disc: 'Authentic Thai Street Food by Chef Sajja Adulet in Charlotte',
+    url: 'https://clinton-fezeu.github.io/curbsidethai/',
+  },
+  {
+    img: '/portfolio.PNG',
+    disc: 'Portfolio of Clinton Fezeu using W3CSS',
+    url: 'https://clinton-fezeu.github.io/portfolio/',
+  },
+];
+
+var settings = {
+  className: 'center',
+  centerMode: true,
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 990,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+        centerMode: false,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+        centerMode: false,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: false,
+      },
+    },
+  ],
+};
+const SliderComp = () => {
+  const arrowRef = useRef(null);
+  let sliderProject = '';
+  sliderProject = data.map((item, i) => <Project item={item} key={i} />);
+  return (
+    <Container>
+      <Slider ref={arrowRef} {...settings}>
+        {sliderProject}
+      </Slider>
+      <Buttons>
+        <button onClick={() => arrowRef.current.slickPrev()} className="back">
+          <IoIosArrowBack />
+        </button>
+        <button onClick={() => arrowRef.current.slickNext()} className="next">
+          <IoIosArrowForward />
+        </button>
+      </Buttons>
+    </Container>
+  );
+};
+
+export default SliderComp;
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const Buttons = styled.div`
+  button {
+    width: 2rem;
+    height: 2rem;
+    background-color: rgba(255, 255, 255, 0.1);
+    cursor: pointer;
+    color: #01be96;
+    border: none;
+    position: absolute;
+    top: 45%;
+    right: -1rem;
+  }
+
+  .back {
+    left: -1rem;
+  }
+`;
